22078aad1b38365dea7bf6d39be4244928f5b0d1
신석용 <syshin@secern.ai>
2025-12-10 00:35:59 +0900

    코드 최신화

--- convert_to_onnx.py
+++ convert_to_onnx.py
@@ -2,16 +2,13 @@ import os
 
 import cv2
 import numpy as np
-import onnx
-import onnx.shape_inference
-import onnxruntime.tools.symbolic_shape_infer
-import onnxsim
+import onnxruntime
+import onnxslim
 import torch
 import torch.nn as nn
-import torch.utils.model_zoo
-import torchvision
+from torch import Tensor
 
-from data import cfg_re50, cfg_mnet
+from data import cfg_mnet, cfg_re50
 from layers.functions.prior_box import priorbox
 from models.retinaface import RetinaFace
 from utils.box_utils import decode, decode_landm
@@ -21,22 +18,22 @@ class RetinaFaceWrapper(nn.Module):
     def __init__(self, cfg: dict, img_size: int):
         super().__init__()
         self.cfg = cfg
-        match self.cfg['name']:
-            case 'Resnet50':
-                state_dict = torch.utils.model_zoo.load_url(
-                    'https://github.com/syshin-cubox-ai/FD_RetinaFace/releases/'
-                    'download/v0.0.1-weights/Resnet50_Final.pth'
+        match self.cfg["name"]:
+            case "Resnet50":
+                state_dict = torch.hub.load_state_dict_from_url(
+                    "https://github.com/syshin-cubox-ai/FD_RetinaFace/releases/"
+                    "download/v0.0.1-weights/Resnet50_Final.pth"
                 )
-            case 'mobilenet0.25':
-                state_dict = torch.load('weights/mobilenet0.25_Final.pth')
+            case "mobilenet0.25":
+                state_dict = torch.load("weights/mobilenet0.25_Final.pth")
             case _:
-                raise ValueError('Wrong cfg')
+                raise ValueError("Wrong cfg")
 
-        self.model = RetinaFace(self.cfg, phase='test')
+        self.model = RetinaFace(self.cfg, phase="test")
         self.model.load_state_dict(state_dict)
         self.priors = priorbox(
-            min_sizes=self.cfg['min_sizes'],
-            steps=self.cfg['steps'],
+            min_sizes=self.cfg["min_sizes"],
+            steps=self.cfg["steps"],
             clip=False,
             image_size=(img_size, img_size),
         )
@@ -44,47 +41,49 @@ class RetinaFaceWrapper(nn.Module):
         self.conf_thres = 0.9
         self.iou_thres = 0.5
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+    def forward(self, x: Tensor) -> Tensor:
         loc, conf, landm = self.model(x)
         loc, conf, landm = loc.squeeze(0), conf.squeeze(0), landm.squeeze(0)
 
         # Decode
-        boxes = decode(loc, self.priors, self.cfg['variance'])
+        boxes = decode(loc, self.priors, self.cfg["variance"])
         boxes = boxes * self.scale
         scores = conf[:, 1]
-        landmarks = decode_landm(landm, self.priors, self.cfg['variance'])
+        landmarks = decode_landm(landm, self.priors, self.cfg["variance"])
         landmarks = landmarks * self.scale
 
-        # Ignore low scores
-        keep = torch.nonzero(scores > self.conf_thres).squeeze(1)
-        boxes = boxes[keep]
-        scores = scores[keep]
-        landmarks = landmarks[keep]
+        # # Ignore low scores
+        # keep = torch.nonzero(scores > self.conf_thres).squeeze(1)
+        # boxes = boxes[keep]
+        # scores = scores[keep]
+        # landmarks = landmarks[keep]
 
-        # NMS
-        keep = torchvision.ops.nms(boxes, scores, self.iou_thres)
-        boxes = boxes[keep]
-        scores = scores[keep]
-        landmarks = landmarks[keep]
+        # # NMS
+        # keep = torchvision.ops.nms(boxes, scores, self.iou_thres)
+        # boxes = boxes[keep]
+        # scores = scores[keep]
+        # landmarks = landmarks[keep]
 
         out = torch.cat((boxes, scores.unsqueeze(1), landmarks), 1)
         return out
 
 
-def convert_onnx(model, img, output_path, opset=17, dynamic=False, simplify=True):
-    assert isinstance(model, torch.nn.Module)
-
+def convert_onnx(
+    model: nn.Module,
+    img: Tensor,
+    output_path: str,
+    opset=18,
+    dynamic=False,
+):
     model.eval()
-    print("[in progress] torch.onnx.export...")
 
     # Define input and output names
-    input_names = ['image']
-    output_names = ['pred']
+    input_names = ["image"]
+    output_names = ["pred"]
 
     # Define dynamic_axes
     if dynamic:
-        dynamic_axes = {input_names[0]: {0: 'N'},
-                        output_names[0]: {0: 'N'}}
+        dynamic_axes = {input_names[0]: {0: "N"}, output_names[0]: {0: "N"}}
     else:
         dynamic_axes = None
 
@@ -92,7 +91,7 @@ def convert_onnx(model, img, output_path, opset=17, dynamic=False, simplify=True
     os.makedirs(os.path.dirname(output_path), exist_ok=True)
     torch.onnx.export(
         model,
-        img,
+        (img,),
         output_path,
         input_names=input_names,
         output_names=output_names,
@@ -100,46 +99,35 @@ def convert_onnx(model, img, output_path, opset=17, dynamic=False, simplify=True
         dynamic_axes=dynamic_axes,
     )
 
-    # Check exported onnx model
-    onnx_model = onnx.load(output_path)
-    onnx.checker.check_model(onnx_model, full_check=True)
-    try:
-        onnx_model = onnxruntime.tools.symbolic_shape_infer.SymbolicShapeInference.infer_shapes(onnx_model)
-        onnx.save(onnx_model, output_path)
-    except Exception as e:
-        print(f'ERROR: {e}, skip symbolic shape inference.')
-    onnx.shape_inference.infer_shapes_path(output_path, output_path, check_type=True, strict_mode=True, data_prop=True)
+    # Simplify ONNX model
+    onnxslim.slim(output_path)
 
     # Compare output with torch model and ONNX model
     torch_out = model(img).detach().numpy()
-    session = onnxruntime.InferenceSession(output_path, providers=['CPUExecutionProvider'])
+    session = onnxruntime.InferenceSession(
+        output_path, providers=["CPUExecutionProvider"]
+    )
     onnx_out = session.run(None, {input_names[0]: img.numpy()})[0]
     try:
         np.testing.assert_allclose(torch_out, onnx_out, rtol=1e-3, atol=1e-4)
     except AssertionError as e:
         print(e)
-        stdin = input('Do you want to ignore the error and proceed with the export ([y]/n)? ')
-        if stdin == 'n':
+        stdin = input(
+            "Do you want to ignore the error and proceed with the export ([y]/n)? "
+        )
+        if stdin == "n":
             os.remove(output_path)
             exit(1)
-
-    # Simplify ONNX model
-    if simplify:
-        model = onnx.load(output_path)
-        input_shapes = {model.graph.input[0].name: img.shape}
-        model, check = onnxsim.simplify(model, test_input_shapes=input_shapes)
-        assert check, 'Simplified ONNX model could not be validated'
-        onnx.save(model, output_path)
-    print(f'Successfully export ONNX model: {output_path}')
+    print(f"Successfully export ONNX model: {output_path}")
 
 
-if __name__ == '__main__':
-    img = cv2.imread('curve/debug.jpg').astype(np.float32)
+if __name__ == "__main__":
+    img = cv2.imread("curve/debug.jpg").astype(np.float32)
     img -= (104, 117, 123)
     img = img.transpose(2, 0, 1)
     img = torch.from_numpy(img).unsqueeze(0)
 
     model = RetinaFaceWrapper(cfg_re50, img.shape[2])
-    # model = RetinaFaceWrapper(cfg_mnet, img.shape[2])
-
-    convert_onnx(model, img, 'onnx_files/retinaface.onnx')
+    convert_onnx(model, img, "onnx_files/retinaface_resnet50.onnx")
+    model = RetinaFaceWrapper(cfg_mnet, img.shape[2])
+    convert_onnx(model, img, "onnx_files/retinaface_mobilenet025.onnx")
